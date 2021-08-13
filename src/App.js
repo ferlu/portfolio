1b648f4eaac273058a3e5b2f9283c152a4eeff6f
Fernanda Luna <ferluna.rm@gmail.com>
2021-08-12 23:21:11 -0500

    * Deploy to gh pages * Remove default files (favicon, logos) * Add portfolio description

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi✨</h1>
+      <p>My name is Fernanda Luna and this is my portfolio. 🌸</p>
+      <p>
+        As you can tell, it's not finished YET. It looks a bit rough, right?
+        It'll look better by the end of August.
+      </p>
+
+      <p style={{ fontWeight: "bold" }}>
+        This is my road to becoming a very very good frontend developer, and
+        there's no better way to learn than to CREATE.
+      </p>
+      <hr></hr>
+      <h2>Quick about me</h2>
+      <p>
+        <ul>
+          <li>Pronouns: She/Her</li>
+          <li>I'm from Mexico</li>
+          <li>
+            I love frontend development! I like to create a good experiences
+            with clean designs.
+          </li>
+          <li>
+            I really like to learn new things, I'm proactive, empathetic and a
+            good teammate.
+          </li>
+          <li>I love traveling, drawing and learning new languages.</li>
+        </ul>
+      </p>
+      <hr></hr>
+      <h2>My projects</h2>
+      <p>
+        Now onto the interesting part...please take a look at the things I've
+        been working on. <br></br> Keep in mind everything here is stuff I code
+        while following tutorials, bootcamps, videos, sometimes I do projects
+        just because of plain inspiration, so they might look a bit messy.
+      </p>
+      <ul>
+        <li>
+          <a href="https://ferlu.github.io/react-todo-app/">React Todo App</a>
+        </li>
+        <li>
+          <a href="https://ferlu.github.io/ig-react/">React Instagram Clone</a>
+        </li>
+        <li>
+          <a href="https://ferlu.github.io/meme-generator/">Meme Generator</a>
+        </li>
+      </ul>
     </div>
   );
 }
